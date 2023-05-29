99097a01e67bc05021b763692fa72fbfb1be4de7
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-05-29 22:12:41 +0200

    Kleiner rewrite, Logik verbessert
    Bands werden jetzt richtig erkannt, aber für einzelne Künstler
    scheint die Logik nach wie vor nicht ganz richtig zu sein;
    die richtige Seite wird (zumindest glaube ich das) gefunden,
    das Tag aber nicht richtig getroffen

--- scripts/get-genre-prototype.py
+++ scripts/get-genre-prototype.py
@@ -5,47 +5,55 @@ from pywikibot import pagegenerators
 import csv
 
 def get_band_genre(band_name):
-    site = pywikibot.Site('en', 'wikipedia')  # English Wikipedia
-    pages = site.search(band_name)  # Search for the band name and get a list of pages
+    site = pywikibot.Site("en", "wikipedia") # Suche auf der englischen Wikipedia-Seite
 
-    # page = pywikibot.Page(site, band_name)
-    # item = pywikibot.ItemPage.fromPage(page)  # this can be used for any page object
+    # Zuerst simpler Suchansatz auf der ersten Treffer-Seite
+    page = pywikibot.Page(site, band_name)
+    item = pywikibot.ItemPage.fromPage(page)
 
-    for page in pages:
-        item = pywikibot.ItemPage.fromPage(page)  # Get the associated Wikidata item
-        item_dict = item.get()  # Get the item dictionary
+    # Erster Suchversuch nach dem Tag "P136"
+    try:
+       item_dict = item.get() # Holt das item dictionary
+       genres = item_dict["claims"]["P136"] # dictionary besitzt tiefe Struktur
+    except KeyError:
+        # Zweiter Suchversuch nach dem Tag "P136"
+        pages = site.search(band_name)
+        print(f"Erste Suche nach P136 auf der Topseite zu {band_name} fehlgeschlagen")
 
-        # Check if the item is an instance of human (Q5) and has occupation (P106) as musician (Q639669)
-        if ('P31' in item_dict['claims'] and any(claim.getTarget().getID() == 'Q5' for claim in item_dict['claims']['P31'])) and \
-            ('P106' in item_dict['claims'] and any(claim.getTarget().getID() == 'Q639669' for claim in item_dict['claims']['P106'])):
-            # The item is an instance of human and musician, so it's likely the band we're looking for
+        for result in pages:
+            item = pywikibot.ItemPage.fromPage(result) # Wikidata-Item von
+            # der jew. Seite ziehen
+            item_dict = item.get() # Holt wieder das item dictionary
             try:
-                # 'P136' is the property for 'Genre'
-                genres = item_dict['claims']['P136']
+                genres = item_dict["claims"]["P136"] # dictionary besitzt tiefe Struktur
             except KeyError:
-                print(f"Für {band_name} ist kein Genre-Tag unter P136 vorhanden, probiere P101")
-                try:
-                    # "P101" ist die Eigenschaft "artists field of work"
-                    genres = item_dict["claims"]["P101"]
-                    continue
-                except KeyError:
-                    print(f"Für {band_name} ist auch kein Genre-Tag unter P101 vorhanden")
-                    # csv für Künstler, bei denen P136 und P101 nicht vorhanden ist
-                    with open("missing_p136p101.csv", "a", newline="") as f:
-                        writer = csv.writer(f)
-                        writer.writerow([band_name])
-                        genres = []  # Kein Genre-Tag vorhanden
-        else:
-            print(f"{page.title()} ist keine Instanz eines Menschen und Künstlers")
-            genres = []  # Kein Genre-Tag vorhanden
+                # Dritter Suchversuch, diesmal nach dem Tag "P106"
+                # Es muss eine Instanz eines Menschen + Beruf des Künstlers gegeben
+                # sein, sonst bedeutet "P106" etwas anderes
+                # Instanz eines Menschen: Q5, Beruf des Künstlers: Q639669
+                if ('P31' in item_dict['claims'] and any(claim.getTarget().getID() == 'Q5' for claim in item_dict['claims']['P31'])) and \
+                   ('P106' in item_dict['claims'] and any(claim.getTarget().getID() == 'Q639669' for claim in item_dict['claims']['P106'])):
+                   try:
+                       genres = item_dict["claims"]["P101"]
+                   except KeyError:
+                       print(f"Für {band_name} ist auch kein Genre-Tag unter P101 vorhanden")
+                       # csv für Künstler, bei denen P136 und P101 nicht vorhanden ist
+                       with open("missing_p136p101.csv", "a", newline="") as f:
+                           writer = csv.writer(f)
+                           writer.writerow([band_name])
+                           genres = []  # Kein Genre-Tag vorhanden
+                       break
+                else:
+                    print(f"{page.title()} ist keine Instanz eines Menschen und Künstlers")
+                    genres = []  # Kein Genre-Tag vorhanden
 
-        for genre in genres:
-            target = genre.getTarget()
-            print(target.labels['en'])
-        break
-
+    for genre in genres:
+        target = genre.getTarget()
+        print(target.labels['en'])
 
 print("Prince:")
 get_band_genre("Prince")
 print("Metallica:")
 get_band_genre("Metallica")
+print("Gorillaz:")
+get_band_genre("Gorillaz")
