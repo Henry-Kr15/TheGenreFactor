cc78b4a908311793144e74fe3913249426934048
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2023-05-30 16:05:29 +0200

    Versuch Genre mit schleife über Wiki Seiten zu finden (nicht so erfolgreich)

--- scripts/get-genre-prototype.py
+++ scripts/get-genre-prototype.py
@@ -3,31 +3,56 @@
 import pywikibot
 from pywikibot import pagegenerators
 import csv
+from pywikibot.exceptions import NoPageError
 
 def get_band_genre(band_name):
     site = pywikibot.Site("en", "wikipedia") # Suche auf der englischen Wikipedia-Seite
+    i=0
+    genres = None
 
-    # Zuerst simpler Suchansatz auf der ersten Treffer-Seite
-    page = pywikibot.Page(site, band_name)
-    item = pywikibot.ItemPage.fromPage(page)
+    # Schleife zur Suche in verschiedenen Artikeln
+    print("erster Versuch")
+    while genres is None:
+        i += 1
+        if(i==10):
+            print("Nach {i} Durchläufen nichts gefunden")
+            break
 
-    # Erster Suchversuch nach dem Tag "P136"
-    # TODO Detaillierterer Fehlerabfang (z.B. mit logging) könnte hier aufschlussreich sein
-    try:
-       item_dict = item.get() # Holt das item dictionary
-       genres = item_dict["claims"]["P136"] # dictionary besitzt tiefe Struktur
-    except KeyError:
-        # Zweiter Suchversuch nach dem Tag "P136"
-        pages = site.search(band_name)
-        print(f"Erste Suche nach P136 auf der Topseite zu {band_name} fehlgeschlagen")
+        # Erster Suchversuch nach dem Tag "P136"
+        # TODO Detaillierterer Fehlerabfang (z.B. mit logging) könnte hier aufschlussreich sein
+        try:
+            page = pywikibot.Page(site, band_name)
+            item = pywikibot.ItemPage.fromPage(page)
+            item_dict = item.get() # Holt das item dictionary
+            genres = item_dict["claims"]["P136"] # dictionary besitzt tiefe Struktur
+        except (KeyError, NoPageError):
+            # Kein Genre-Tag gefunden oder Seite nicht vorhanden
+            # Nächste Seite ausprobieren
+            print(f"Erste Suche nach P136 auf der {i}ten Seite zu {band_name} fehlgeschlagen")
+            try:
+                page = next(pywikibot.pagegenerators.SearchPageGenerator(band_name, site=site))
+                item = pywikibot.ItemPage.fromPage(page)
+            except StopIteration:
+                print(f"Kein Genre-Tag gefunden für {band_name}")
+#                break  # Schleife beenden, wenn alle Artikel durchsucht wurden
 
-        for result in pages:
+    if genres is None:
+        for result in pywikibot.pagegenerators.SearchPageGenerator(band_name, site=site):
             item = pywikibot.ItemPage.fromPage(result) # Wikidata-Item von
             # der jew. Seite ziehen
             item_dict = item.get() # Holt wieder das item dictionary
             try:
-                genres = item_dict["claims"]["P136"] # dictionary besitzt tiefe Struktur
+                print("zweiter Versuch")
+                if "P136" in item_dict["claims"]:
+                    genres = item_dict["claims"]["P136"] # dictionary besitzt tiefe Struktur
+                    print("2.1")
+                    break
+                elif "P495" in item_dict["claims"]:
+                    genres = item_dict["claims"]["P495"]
+                    print("2.2")
+                    break
             except KeyError:
+                print("dritter Versuch")
                 # Dritter Suchversuch, diesmal nach dem Tag "P106"
                 # Es muss eine Instanz eines Menschen + Beruf des Künstlers gegeben
                 # sein, sonst bedeutet "P106" etwas anderes
@@ -35,7 +60,9 @@ def get_band_genre(band_name):
                 if ('P31' in item_dict['claims'] and any(claim.getTarget().getID() == 'Q5' for claim in item_dict['claims']['P31'])) and \
                    ('P106' in item_dict['claims'] and any(claim.getTarget().getID() == 'Q639669' for claim in item_dict['claims']['P106'])):
                    try:
-                       genres = item_dict["claims"]["P101"]
+                        if "P101" in item_dict["claims"]:
+                            genres = item_dict["claims"]["P101"]
+                            break
                    except KeyError:
                        print(f"Für {band_name} ist auch kein Genre-Tag unter P101 vorhanden")
                        # csv für Künstler, bei denen P136 und P101 nicht vorhanden ist
@@ -43,13 +70,11 @@ def get_band_genre(band_name):
                        with open("missing_p136p101.csv", "a", newline="") as f:
                            writer = csv.writer(f)
                            writer.writerow([band_name])
-
-                       genres = []  # Kein Genre-Tag vorhanden
                        break
                 else:
                     print(f"{page.title()} ist keine Instanz eines Menschen und Künstlers")
-                    genres = []  # Kein Genre-Tag vorhanden
 
+#    return genres
     for genre in genres:
         target = genre.getTarget()
         print(target.labels['en'])
