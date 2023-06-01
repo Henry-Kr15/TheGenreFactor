e9a4f9ccebe2d83d7896c2df8aea1f7e3d16ce25
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-06-01 14:38:39 +0200

    Klappt halbwegs

--- scripts/get_genre_prototype_rewrite.py
+++ scripts/get_genre_prototype_rewrite.py
@@ -7,34 +7,29 @@ from typing import Dict
 
 
 def get_genre(band_name: str, album_name: str, album_type: str) -> Dict:
-    site = pywikibot.Site("en", "wikipedia")  # Suche auf der englischen Wikipedia-Seite
     genre_dict = {}
 
     # Zuerst Überprüfung, ob sich um Single oder Album handelt
     if album_type == "single" or album_type == "album":
         # 1. Suche nach Single/Album + band_name
         try:
-            search_name = album_name + " " + band_name + f" (album_type)"
-            pages = site.search(search_name)
-            genre_dict = site_search(pages)
+            search_name = album_name + " " + band_name + f" ({album_type})"
+            genre_dict = site_search(search_name)
         except (KeyError, NoPageError):
             try:
                 # 2. Suche nach Single/Album
-                search_name = album_name + " (album_type)"
-                pages = site.search(search_name)
-                genre_dict = site_search(pages)
+                search_name = album_name + f" ({album_type})"
+                genre_dict = site_search(search_name)
             except (KeyError, NoPageError):
                 try:
                     # 3. Nur Suche nach dem Namen des Liedes
                     search_name = album_name
-                    pages = site.search(search_name)
-                    genre_dict = site_search(pages)
+                    genre_dict = site_search(search_name)
                 except (KeyError, NoPageError):
                     try:
                         # 4. Nur Suche nach dem Namen der Band
                         search_name = band_name
-                        pages = site.search(search_name)
-                        genre_dict = site_search
+                        genre_dict = site_search(search_name)
                     except (KeyError, NoPageError):
                         print(
                             f"Suche nach {album_name} mit allen Zusätzen fehlgeschlagen; zu den Suchbegriffen gab es keine Ergebnisse"
@@ -57,17 +52,20 @@ def get_genre(band_name: str, album_name: str, album_type: str) -> Dict:
     return genre_dict
 
 
-def site_search(pages) -> Dict:
+def site_search(search_name) -> Dict:
     genres = None
-    for page in pages:
-        # Erster Suchversuch "P136" auf der Album-Seite
-        try:
-            item = pywikibot.ItemPage.fromPage(page)
-            item_dict = item.get()
-            genres = item_dict["claims"]["P136"]
-            print(f"Suche nach P136 auf Seite {page} erfolgreich")
-        except (KeyError, NoPageError):
-            continue
+    site = pywikibot.Site("en", "wikipedia")
+    page = pywikibot.Page(site, search_name)
+
+    while genres is None:
+        item = pywikibot.ItemPage.fromPage(page)
+        item_dict = item.get()
+        genres = item_dict["claims"]["P136"]
+        page = next(
+            pywikibot.pagegenerators.SearchPageGenerator(search_name, site=site)
+        )
+
+    print(page)
 
     # Jetzt noch Genres in Dict schreiben
     genre_dict = {}
