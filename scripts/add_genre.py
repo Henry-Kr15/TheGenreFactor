a29fbc4973d44e799d0497ae3f00f72e85eb952e
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-06-02 12:28:42 +0200

    Cleanup

--- scripts/add_genre.py
+++ scripts/add_genre.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-# from get_genre_prototype import get_band_genre, clear_csv
-from get_genre_prototype_rewrite import get_genre
+from get_genre import get_genre
 
 print(get_genre("Metallica", "Ride the Lightning", "album"))
 print(get_genre("Bring me the Horizon", "Sempiternal", "album"))
