310ff00de854f5d04a0a627d3dc3c3700318ed0c
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2023-06-02 09:36:34 +0200

    mehr künstler

--- scripts/add_genre.py
+++ scripts/add_genre.py
@@ -4,5 +4,9 @@ from get_genre_prototype_rewrite import get_genre
 
 print(get_genre("Metallica", "Ride the Lightning", "album"))
 print(get_genre("Bring me the Horizon", "Sempiternal", "album"))
-print(get_genre("Lorna Shore", "Pain Remains", "single"))
+print(get_genre("Lorna Shore", "Pain Remains", "album"))
 print(get_genre("Gorillaz", "New Gold (feat. Tame Impala and Bootie Brown)", "single"))
+print(get_genre("Ludovico Einaudi", "Una Mattina", "album"))
+print(get_genre("Rolf Zuckowski", "Theo", "single"))
+print(get_genre("Death", "Scream Bloody Gore", "album"))
+print(get_genre("Movie", "Evil Dead", "album")) # nice dass das auch mit Filmen klappt :D
