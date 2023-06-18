5783a370efc2688c969c1397c2f047c860a20d10
Henry Krämerkämper <henry.kraemerkaemper@tu-dortmund.de>
2023-06-18 16:22:40 +0200

    Arbeit an Feature Scaling

---
+++ scripts/modular_NN_chain/explore_data_functions.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from pandas.plotting import scatter_matrix
+from sklearn.preprocessing import QuantileTransformer
+
+
+def plot_columns(df):
+    numeric_df = df.select_dtypes(include=np.number)  # select only numeric columns
+    if "Unnamed: 0" in numeric_df.columns:
+        numeric_df = numeric_df.drop(columns=["Unnamed: 0"])
+    for column in numeric_df.columns:
+        print(column)
+        plt.figure()
+        if (
+            column == "Views"
+            or column == "Likes"
+            or column == "Comments"
+            or column == "Stream"
+            or column == "Duration_ms"
+        ):
+            bins = np.logspace(0, np.log10(numeric_df[column].max()), 20)
+            sns.histplot(numeric_df[column], bins=bins, kde=False)
+            plt.xscale("log")
+        elif column == "Instrumentalness":
+            sns.histplot(numeric_df[column], bins=20, kde=True)
+            plt.yscale("log")
+        else:
+            sns.histplot(numeric_df[column], bins=20, kde=True)
+        plt.title(f"Distribution of {column}")
+        plt.xlabel(column)
+        plt.ylabel("Count")
+        plt.savefig(f"../figures/Hist-{column}.pdf")
+        plt.close()
+
+
+def visualize_features(df: pd.DataFrame, Folder_name: str):
+
+    plot_columns(df)
+
+    print(df.head())
+    print("---")
+    print(df.describe())
+
+    # Scatterplot
+    print("Creating scatterplot...")
+    numeric_df = df.select_dtypes(include=np.number)
+    selected_columns = numeric_df.iloc[:, 1:16]
+    sampled_data = selected_columns.sample(frac=1.0)
+    scatter_matrix = pd.plotting.scatter_matrix(
+        sampled_data, alpha=0.8, figsize=(10, 10), s=10, hist_kwds={"bins": 20}
+    )
+    for ax in scatter_matrix.ravel():
+        ax.xaxis.label.set_rotation(90)
+        ax.yaxis.label.set_rotation(0)
+        ax.yaxis.label.set_ha("right")
+    plt.savefig(f"../figures/{Folder_name}/scatter.png")
+
+    # Correlation
+    print("Creating correlation plot...")
+    plt.figure(figsize=(20, 20))
+    sns.heatmap(selected_columns.corr(), annot=True, square=True, cmap="coolwarm")
+    plt.savefig(f"../figures/{Folder_name}/correlation.pdf")
+
+
+# df = pd.read_csv("../data/Spotify_Youtube.csv")
+# df = pd.read_csv("../data/data_selected_v1.csv")
+
+# visualize_features(df, "Test1")
--- scripts/modular_NN_chain/training_v1.py
+++ scripts/modular_NN_chain/training_v1.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
 import pandas as pd
-import sklearn
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import QuantileTransformer
+from explore_data_functions import visualize_features
+
 
 df = pd.read_csv("../../data/data_selected_v1.csv", index_col=0)
 
@@ -9,3 +13,25 @@ df = pd.read_csv("../../data/data_selected_v1.csv", index_col=0)
 X = df.drop("Genre", axis=1)
 y = df["Genre"]
 
+# Genre Kodieren
+genre_encoder = OneHotEncoder(sparse_output=False)
+one_hot_y = genre_encoder.fit_transform(y.unique().reshape(-1,1)) # muss wieder in Dataframe umgewandelt werden
+y = pd.DataFrame(one_hot_y, columns=genre_encoder.get_feature_names_out())
+
+# Datensatz aufteilen, mit shuffle und stratify
+# Trainingsatz:     64%
+# Testsatz:         20%
+# validierungssatz: 16%
+
+# Trainings- und Testsatz
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=True, random_state=42)
+
+# Validierungssatz für die Hyperparameteroptimierung
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, stratify=True , random_state=42)
+
+
+# Skalierung auf dem Trainigssatz und Anwendung dieser Skalierung auf den Test- und Validierungssatz
+# Was sollte die Skalierung tun?
+#
+# Neuronale Netze "mögen" Wertebereiche mit mean=0, daher auf [-1,1] skalieren
+# Jede Verteilung sollte letztlich Gauß-änhlich sein?
